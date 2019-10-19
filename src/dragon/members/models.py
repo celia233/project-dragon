a0eee49d15b2c8546f0a7886030a8c36093088da
celia233 <50320136+celia233@users.noreply.github.com>
2019-10-19 21:07:39 +0800

    Made some field of members unique
    made email, student number, phone number unique

--- src/dragon/members/models.py
+++ src/dragon/members/models.py
@@ -4,34 +4,45 @@ import django.utils
 from django.db import models
 #from guardian.models import GroupObjectPermissionBase
 
+
 class User(Model):
     id = AutoField(primary_key=True)
     FirstName = CharField(max_length=50)
     surname = CharField(max_length=50)
-    email = EmailField(default='')
-    phoneNumber = CharField(max_length=20, default='')
+    email = EmailField(default='',unique=True)
+    phoneNumber = CharField(max_length=20, default='',unique=True)
 
     def __str__(self):
-        return self.id
+        return self.FirstName
+        #return self.id
 
 
 class ClubMember(User):
 
-    password = CharField(max_length=20)
     preferredName = CharField(max_length=50, default='')
     preferredPronoun = CharField(max_length=20, default='')
     guildMember = BooleanField(default=True)
     isStudent = BooleanField(default=True)
-    universityID = CharField(max_length=8, default='')
+    universityID = CharField(max_length=8, default='',unique=True)
     joinDate = DateField(default=django.utils.timezone.now)
     incidents = TextField(max_length=400, default='N/A')
+    
+    rankChoices = [
+        ('MEM', 'Member'), 
+        ('GK', 'Gate Keeper'), 
+        ('OCM', 'Ordinary Committee'), 
+        ('EXEC', 'Club Executive')]
+    clubRank = TextField(choices=rankChoices, default='MEM')
+
 
     def __str__(self):
-        return self.id
+        return self.FirstName
+        #return self.id
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
+
 class NonMember(User):
 
     organization = CharField(max_length=200)
@@ -39,7 +50,7 @@ class NonMember(User):
     incidents = TextField(max_length=400, default='N/A')
 
     def __str__(self):
-        return self.id
+        return self.FirstName
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
